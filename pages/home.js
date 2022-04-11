ea02758a03d8feb2913ccffd3f7473ed8609832d
MisterMikeTee <miguel.tome162@gmail.com>
2022-04-11 18:16:38 +0100

    creating home && user profile page; trying to add bootstrap

---
+++ pages/home.js
@@ -0,0 +1,15 @@
+import styles from '../styles/HomePage.module.css'
+import Head from 'next/head'
+
+export default function homePage() {
+    return (
+    <>
+        <div className={styles.navbarContainer}>
+            <h1 className={styles.navbarTitle}>Title</h1>
+         </div>
+        <main>
+            
+        </main>
+    </>
+    )
+}
---
+++ pages/user/user-profile.js
@@ -0,0 +1,6 @@
+
+export default function userProfile() {
+    return (
+        <div className="profile-title">Profile Title</div>
+    )
+}

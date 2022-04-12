90ea3728f703efbfb66f7adceb4385e7ab753eac
MisterMikeTee <miguel.tome162@gmail.com>
2022-04-12 21:42:33 +0100

    reworking structure; trying to get side menu button work

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,15 @@
+import Layout from '../components/Layout';
 import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import Head from 'next/head';
 import { useEffect } from "react";
-import styles from '../styles/globalStyles.module.css'
+
 
 function MyApp({ Component, pageProps }) {
   
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
-  
   return (
-  <> 
-    <Head>
-      <div className={styles.navbarContainer}>
-        <div className = 'col-4'>
-          <button type="button" style={{backgroundColor: 'transparent', border: 'none', outline: 'none'}} className="btn btn-secondary">Menu</button>
-        </div>
-        <div className='col-4'>
-          <h1 className={styles.navbarTitle}>Title</h1>
-        </div>
-        <div className='col-4'>
-
-        </div>
-      </div>
-    </Head>
-    <Component {...pageProps} />
-  </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
--- pages/home.js
+++ pages/home.js
@@ -3,18 +3,7 @@ import styles from '../styles/HomePage.module.css'
 export default function homePage() {
     return (
     <>
-        <main>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </main>
+    
     </>
     )
 }

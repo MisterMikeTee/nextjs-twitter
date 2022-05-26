00db67807d4863ac65c9069ac9027efee52cb2df
MisterMikeTee <miguel.tome162@gmail.com>
2022-05-26 22:15:25 +0100

    improving method on homepage posts to show mutiple posts; working on visuals

--- components/Post.js
+++ components/Post.js
@@ -8,28 +8,32 @@ import { red } from '@mui/material/colors';
 
 export default function post ({info}) {
 
-    let name = info.author;
-    let array = name.split(' ');
-    let first_name = array[0];
-    let last_name = array[array.length - 1];
-    let initals = first_name[0] + last_name[0]
+    info.forEach(element => {
+        let name = element.author;
+        let array = name.split(' ');
+        let first_name = array[0];
+        let last_name = array[array.length - 1];
+        let initals = first_name[0] + last_name[0]
+        element.initals = initals 
+    });
 
-    return (
-        <Card sx={{maxWidth: 900, width: "90%" }}>
-            <CardHeader
-                avatar = {
-                    <Avatar sx={{bgColor: red[500]}}>
-                        {initals}
-                    </Avatar>
-                }
-                title={info.title}
-                subheader= {info.date}
-            />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    {info.text}
-                </Typography>
-            </CardContent>
-        </Card>
+
+    return info.map((el) =>
+        <Card sx={{maxWidth: 900, width: "90%" , margin: "1.5rem auto"}}>
+        <CardHeader
+            avatar = {
+                <Avatar sx={{bgColor: red[500]}}>
+                    {el.initals}
+                </Avatar>
+            }
+            title={el.title}
+            subheader= {el.date}
+        />
+        <CardContent>
+            <Typography variant="body2" color="text.secondary">
+                {el.text}
+            </Typography>
+        </CardContent>
+    </Card>
     )
 }
--- components/PostList.js
+++ components/PostList.js
@@ -3,12 +3,19 @@ import styles from '../styles/PostList.module.css'
 
 export default function postList () {
 
-    const post = {
+    const post = [{
         date: 'May 2 2022 at 23:13',
         text: "This is a nice post, so read it carefuly",
         title: "A nice post",
         author: "Miguel Tomé"
+    },
+    {
+        date: 'May 26 2022 at 21:47',
+        text: "This is a new nice post, so read it carefuly",
+        title: "A nice post",
+        author: "Aurélio Figueira" 
     }
+    ]
     return(
         <div className={styles.postList}>
             <Post info = {post} ></Post>
